d6a978f6dd0e8d9641bb6c880d4b97d572f979d2
Borislav Borislavov <borislavkb@gmail.com>
2021-07-02 15:12:52 +0200

    Add if statement on button click

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const answer = document.querySelector(".Card-answer");
 
 buttonJS.addEventListener("click", () => {
   answer.classList.toggle("Card-answer_hidden");
+  if (buttonJS.textContent === "Show answer") {
+    buttonJS.textContent = "Hide answer";
+  } else {
+    buttonJS.textContent = "Show answer";
+  }
 });
