30502c04b9e0ed84e2255db550dfc1fdc2fec60b
Borislav Borislavov <borislavkb@gmail.com>
2021-07-02 14:10:50 +0200

    Change name on js file and change script link

---
+++ styles/index.js
@@ -0,0 +1,7 @@
+const showAnswerButton = document.querySelector(".button");
+const cardAnswer = document.querySelector(".Card-answer");
+
+showAnswerButton.addEventListener("click", () => {
+  // cardAnswer.style.visibility = "visible";
+  console.log(cardAnswerButton.textContent);
+});
