6e66bd0823b0b6c1a7eed07192100850380133bb
Borislav Borislavov <borislavkb@gmail.com>
2021-07-07 22:04:25 +0200

    Add addeventListerner on button

--- bookmark.js
+++ bookmark.js
@@ -16,13 +16,12 @@ cardComponents.forEach((cardComponent) => {
 const cardAnswer = document.querySelectorAll(".Card-answer");
 const buttonShow = document.querySelectorAll("button");
 
-for (let b = 0; b < buttonShow.length; b++) {
-  buttonShow[b].addEventListener("click", () => {
-    cardAnswer[b].classList.toggle("Card-answer_hidden");
-    if (buttonShow.textContent === "Show answer") {
-      buttonShow.textContent = "Hide answer";
-    } else {
-      buttonShow.textContent = "Show answer";
-    }
-  });
-}
+// for (let b = 0; b < buttonShow.length; b++) {
+//   buttonShow[b].addEventListener("click", () => {
+//     cardAnswer[b].classList.toggle("Card-answer_hidden");
+//     if (buttonShow.textContent === "Show answer") {
+//       buttonShow.textContent = "Hide answer";
+//     } else {
+//       buttonShow.textContent = "Show answer";
+//     }
+//   });
