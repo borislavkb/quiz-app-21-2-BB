1d589eddc831f62d9c7d65a27e3e9c49b73a325c
Borislav Borislavov <borislavkb@gmail.com>
2021-07-07 12:46:53 +0200

    Create dd.js and card.js

---
+++ styles/lib/card.js
@@ -0,0 +1,13 @@
+
+/* Shape of cardData parameter
+{
+    id: 0,
+    question: "What type of a language is HTML?",
+    answer: "Markup Language",
+    isBookmarked: true,
+    tags: ["HTML", "general"],
+  },*/
+
+
+
+export function createCardComponent 
